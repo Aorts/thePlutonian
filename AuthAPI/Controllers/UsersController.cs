e5efa3171faf8b7deb672969efe1cb090ccadc3f
Aorts <gusgame.aor@gmail.com>
2023-02-15 00:06:31 +0700

    Add project files.

---
+++ AuthAPI/Controllers/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using AuthAPI.Context;
+using AuthAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UsersController(AppDbContext authContext)
+        {
+            _authContext = authContext;
+        }
+        [HttpPost]
+        [Route("authen")]
+        public async Task<IActionResult> Authenticate([FromBody] LoginModel userObj)
+        {
+            try
+            {
+                if(userObj == null)
+                {
+                    return BadRequest();
+                }
+                var user = await _authContext.Users.FirstOrDefaultAsync( 
+                    x => x.UserName == userObj.UserName && x.Password == userObj.Password
+                    );
+                if (user == null)
+                {
+                    return NotFound(new { Message = "User Not Found"});
+                }
+                return Ok(new { Message = "Loggin Successful!" });
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            try
+            {
+                if (userObj == null)
+                {
+                    return BadRequest();
+                }
+                await _authContext.Users.AddAsync(userObj);
+                await _authContext.SaveChangesAsync();
+                return Ok(new { Message = "Register Successful!" });
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ AuthAPI/Migrations/20230214164610_v2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthAPI.Migrations
+{
+    public partial class v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
